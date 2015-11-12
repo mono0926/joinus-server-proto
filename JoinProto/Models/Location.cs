6bf0b715b29cdc2c77d53a7c0151e05d0219c2da
Masayuki Ono <mono0926@gmail.com>
2015-11-12 15:46:10 +0900

    Add post user and location request

--- JoinProto/Models/Location.cs
+++ JoinProto/Models/Location.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace JoinProto.Models
 {
@@ -11,8 +12,9 @@ namespace JoinProto.Models
         public int Id { get; set; }
         public double Latitude { get; set; }
         public double Longitude { get; set; }
-        public DateTime Time { get; set; }
+        public DateTime? Time { get; set; }
         public int UserId { get; set; }
+        [JsonIgnore]
         public virtual User User { get; set; }
     }
 }
